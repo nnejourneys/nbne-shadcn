a8444eb1212c165fef74cb4c7087e51687fe7be2
nnejourneys <106916247+nnejourneys@users.noreply.github.com>
2024-01-29 15:03:28 +0530

    Update heading.tsx

--- components/styledcomps/heading.tsx
+++ components/styledcomps/heading.tsx
@@ -1,27 +1,27 @@
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
-import React, { ReactNode } from "react";
+import React from "react";
 
 const headingVariants = cva("text-3xl font-bold mb-14", {
   variants: {
     variant: {
-      default: "text-primary-foreground text-xl font-semibold mb-3",
+      default: "text-foreground text-xl font-semibold mb-3",
       sectiontitle:
         "text-center mb-14 relative before:absolute before:content-[''] before:h-0.5 before:w-32 before:-bottom-2.5 before:left-1/2 before:-translate-x-1/2 before:bg-primary after:absolute after:content-[''] after:h-2.5 after:w-2.5 after:rounded-full after:bg-background after:border-2 after:border-primary after:shadow-[0_0_0_5px_rgba(0,0,0)] after:shadow-background after:-bottom-3.5 after:left-1/2 after:-translate-x-1/2",
       sectiontitlehalf:
         "mb-14 relative before:absolute before:content-[''] before:h-0.5 before:w-16 before:-bottom-2.5 before:left-6 before:bg-primary after:absolute after:content-[''] after:h-2.5 after:w-2.5 after:rounded-full after:bg-transparent after:border-2 after:border-primary after:shadow-[0_0_0_5px_rgba(0,0,0)] after:shadow-transparent after:-bottom-3.5 after:left-2",
       sectiontitlesm:
-        "bg-primary/20 text-primary-foreground shadow",
+        "bg-primary/20 text-foreground shadow",
     },
     size: {
-      default: "text-md md:text-xl",
-      xxl: "text-5xl md:text-8xl",
-      xl: "text-4xl md:text-7xl",
-      lg: "text-3xl md:text-5xl",
-      md: "text-2xl md:text-4xl",
-      sm: "text-xl md:text-3xl",
-      xs: "text-lg md:text-xl",
+      default: "text-md md:text-lg lg:text-xl",
+      xxl: "text-5xl md:text-7xl lg:text-8xl",
+      xl: "text-4xl md:text-6xl lg:text-7xl",
+      lg: "text-3xl md:text-4xl lg:text-5xl",
+      md: "text-2xl md:text-3xl lg:text-4xl",
+      sm: "text-xl md:text-2xl lg:text-3xl",
+      xs: "text-sm md:text-md lg:text-lg",
     },
   },
   defaultVariants: {
