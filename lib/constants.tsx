b4d0b53cc0adb44a79c164aafd36314cb4431411
nnejourneys <106916247+nnejourneys@users.noreply.github.com>
2024-02-02 12:11:34 +0530

    Update constants.tsx

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -1,4 +1,4 @@
-export const BASE_PATH = 'https://nbne-shadcn.vercel.app/'
+export const BASE_PATH = 'https://nbne-shadcn.vercel.app'
 export const SITE_TITLE = 'North by Northeast Journeys'
 export const SITE_DESC = 'We specialize in adventure, nature and culture tours across the little-known Northeast region of India. | North by Northeast Journeys.'
 export const SITE_KEYWORDS = [ "Northeast India", "Assam", "Arunachal Pradesh", "Meghalaya", "Nagaland", "Sikkim", "Active Holidays", "Leisure Holidays", "Family Holidays", "Cycling", "Trekking", "Rafting", "Culture Holidays"]
